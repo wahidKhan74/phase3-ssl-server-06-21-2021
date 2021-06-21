aebdc6b281596be1067b0e0e30fa161484f54076
Waheed Khan <wahid.khan74@gmail.com>
2021-06-21 11:10:09 +0000

    adding self signed ssl cirtificate to spring boot app

---
+++ src/main/java/com/mcit/webapp/Phase3SslServer06212022Application.java
@@ -0,0 +1,13 @@
+package com.mcit.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase3SslServer06212022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase3SslServer06212022Application.class, args);
+	}
+
+}
